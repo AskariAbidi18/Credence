b55f0325f39d461157dc842ec2f1f477808bcc58
Syed Mohammad Askari Abidi <askari.abidi.2005@gmail.com>
2026-08-20 10:46:05 +0530

    feat: make frontend backend URL configurable

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -4,7 +4,9 @@
  * Single source of truth for all backend communication.
  */
 
-export const BACKEND_URL = "http://localhost:8000";
+export const BACKEND_URL =
+  localStorage.getItem("credence_backend_url") ||
+  "http://localhost:8000";
 
 export function getBackendUrl() {
   return (localStorage.getItem("credence_backend_url") || BACKEND_URL).replace(
